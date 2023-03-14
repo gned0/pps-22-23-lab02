9330e133bee65c69d49ea3f6681b0f558e177e20
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2023-03-14 16:44:03 +0100

    completed ex8, prettier prints

--- src/u02/exercises/exercises.scala
+++ src/u02/exercises/exercises.scala
@@ -15,16 +15,14 @@ object exercises extends App{
     case n if n >= 0 => "positive"
     case _ => "negative"
 
-  println("EXERCISE 3A, expected output: (p, p, n, n)")
+  println("EXERCISE 3A")
+  println("Expected: positive, actual: " + positiveMethod(3))
+  println("Expected: negative, actual: " + positiveMethod(-3))
+  println("Expected: positive, actual: " + positiveLambda(3))
+  println("Expected: negative, actual: " + positiveLambda(-3))
   println()
-  println(positiveMethod(3))
-  println(positiveMethod(-3))
 
-  println(positiveLambda(3))
-  println(positiveLambda(-3))
-  println()
   // exercise 3b
-
   val negLambda: (String => Boolean) => String => Boolean =
     f => s => !f(s)
 
@@ -34,18 +32,15 @@ object exercises extends App{
   val empty: String => Boolean = _ == ""
   val notEmpty = negLambda(empty)
 
-  println("EXERCISE 3B, expected output: true")
-  println()
-  println(notEmpty("foo") && !notEmpty(""))
+  println("EXERCISE 3B")
+  println("Expected: true, actual: " + (notEmpty("foo") && !notEmpty("")))
   println()
 
   // exercise 3c
-
   def negMethodGeneric[A](f: A => Boolean): A => Boolean =
     (s: A) => !f(s)
 
   // exercise 4
-
   val p1: Int => Int => Int => Boolean = x => y => z => x <= y && y == z
   val p2: (Int, Int, Int) => Boolean = (x, y, z) => x <= y && y == z
   def p3(x: Int)(y: Int)(z: Int): Boolean =
@@ -53,26 +48,22 @@ object exercises extends App{
   def p4(x: Int, y: Int, z: Int): Boolean =
     x <= y && y == z
 
-  println("EXERCISE 4, expected output: true x4")
-  println()
-  println(p1(6)(8)(8))
-  println(p2(6, 8, 8))
-  println(p3(6)(8)(8))
-  println(p4(6, 8, 8))
+  println("EXERCISE 4")
+  println("Expected: true, actual: " + p1(6)(8)(8))
+  println("Expected: true, actual: " + p2(6, 8, 8))
+  println("Expected: true, actual: " + p3(6)(8)(8))
+  println("Expected: true, actual: " + p4(6, 8, 8))
   println()
 
   // exercise 5
-
   def compose(f: Int => Int, g: Int => Int): Int => Int =
     x => f(g(x))
 
-  println("EXERCISE 5, expected output: 9")
-  println()
-  println(compose(_ - 1, _ * 2)(5))
+  println("EXERCISE 5")
+  println("Expected: 9, actual: " + compose(_ - 1, _ * 2)(5))
   println()
 
   // exercise 6
-
   @tailrec
   def gcd(a: Int, b: Int): Int = (a, b) match
     case (a, b) if a == 0 => b
@@ -84,13 +75,12 @@ object exercises extends App{
       case (a, b) if b % a == 0 => a
       case (a, b) => gcd(a, b % a)
 
-  println("EXERCISE 6, expected output: 4, 7")
+  println("EXERCISE 6")
+  println("Expected output: 4, actual: " + gcd(8, 12))
+  println("Expected output: 7, actual: " + gcd(14, 7))
   println()
-  println(gcd(8, 12))
-  println(gcd(14, 7))
 
   // exercise 7
-
   enum Shape:
     case Triangle(a: Point2D, b: Point2D, c: Point2D)
     case Circle(center: Point2D, radius: Double)
@@ -101,7 +91,7 @@ object exercises extends App{
       sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2))
     def perimeter(s: Shape): Double = s match
       case Triangle(a, b, c) => distance(a, b) + distance(a, c) + distance(b, c)
-      case Circle(c, r) => 2 * 3.14 * r
+      case Circle(c, r) => 2 * java.lang.Math.PI * r
       case Square(o, s) => 4 * s
     def contains(s:Shape, p: Point2D): Boolean = s match
       case Triangle(a, b, c) => true
@@ -117,4 +107,33 @@ object exercises extends App{
   println(perimeter(Square(Point2D(0, 0), 4)))
   println(contains(Circle(Point2D(0, 0), 2), Point2D(4, 4)))
   println(contains(Square(Point2D(0, 0), 2), Point2D(1, 1)))
+
+  // exercise 8
+
+  enum Option[A]:
+    case Some(a: A)
+    case None()
+
+  object Option:
+    def filter[A](opt: Option[A])(f: A => Boolean): Option[A] = opt match
+      case Some(a) if f(a) => Some(a)
+      case _ => None()
+    def map[A, B](opt: Option[A])(f: A => B): Option[B] = opt match
+      case Some(a) => Some(f(a))
+      case _ => None()
+    def fold[A](opt: Option[A])(default: A)(f: A => A): A = opt match
+      case Some(a) => f(a)
+      case _ => default
+
+  import Option.*
+  println("EXERCISE 8, expected output: Some(5), None(), None()")
+  println()
+  println("Expected: Some(5), actual: " + filter(Some(5))(_ > 2))
+  println("Expected: None(), actual: " + filter(Some(5))(_ > 8))
+  println("Expected: None(), actual: " + filter(None[Int]())(_ > 2))
+  println("Expected: Some(true), actual: " + map(Some(5))(_ > 2))
+  println("Expected: Some(false), actual: " + map(Some(5))(_ > 8))
+  println("Expected: None(), actual: " + map(None[Int]())(_ > 2))
+  println("Expected: 6, actual: " + fold(Some(5))(1)(_ + 1))
+  println("Expected: 1, actual: " + fold(None[Int]())(1)(_ + 1))
 }
