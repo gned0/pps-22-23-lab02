39098b0e3a211628bf47d240bf1a63048f816351
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2024-10-04 18:50:02 +0200

    refactor: improve gcd implementation

--- src/u02/exercises/ex6.scala
+++ src/u02/exercises/ex6.scala
@@ -6,14 +6,8 @@ object ex6 extends App {
 
   @tailrec
   def gcd(a: Int, b: Int): Int = (a, b) match
-    case (a, b) if a == 0 => b
-    case (a, b) if b == 0 => a
-    case (a, b) if a > b => (a, b) match
-      case (a, b) if a % b == 0 => b
-      case (a, b) => gcd(b, a % b)
-    case (a, b) if a < b => (a, b) match
-      case (a, b) if b % a == 0 => a
-      case (a, b) => gcd(a, b % a)
+    case (_, 0) => a
+    case _ => gcd(b, a % b)
 
   println(gcd(8, 12))
   println(gcd(14, 7))
