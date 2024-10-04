87e5556df347730091c075f14cfc55efa2cda3fa
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2024-10-04 18:42:15 +0200

    feat: add generic compose version

--- src/u02/exercises/ex5.scala
+++ src/u02/exercises/ex5.scala
@@ -6,5 +6,9 @@ object ex5 extends App {
     x => f(g(x))
 
   println(compose(_ - 1, _ * 2)(5))
-  
+
+  def compose_generic[A, B, C](f: B => C, g: A => B): A => C = x => f(g(x))
+
+  println(compose_generic[Int, Int, Int](_ - 1, _ * 2)(5))
+
 }
